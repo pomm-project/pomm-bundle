2825b4e6379c12e594bfe9fbe524a1d90802848f
Sanpi <sanpi@homecomputing.fr>
2017-04-13 13:58:12 +0200

    [test] Fix annotation autoloader

--- tests/app/autoload.php
+++ tests/app/autoload.php
@@ -6,6 +6,8 @@ use Composer\Autoload\ClassLoader;
 /** @var ClassLoader $loader */
 $loader = require __DIR__.'/../vendor/autoload.php';
 
+/** @see https://github.com/doctrine/annotations/issues/103 */
+AnnotationRegistry::registerLoader('class_exists');
 AnnotationRegistry::registerLoader([$loader, 'loadClass']);
 
 return $loader;
