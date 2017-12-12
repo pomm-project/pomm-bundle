cc2f9cdf38dda888727c8b3a729940ea66e9e621
Mikael Paris <stood86@gmail.com>
2017-12-12 16:44:07 +0100

    add autowire for symfony

--- sources/lib/DependencyInjection/PommExtension.php
+++ sources/lib/DependencyInjection/PommExtension.php
@@ -10,6 +10,7 @@
 namespace PommProject\PommBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Alias;
 use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
@@ -59,7 +60,7 @@ class PommExtension extends Extension
     public function configure(array $config, ContainerBuilder $container)
     {
         $definition = $container->getDefinition('pomm');
-
+        $container->setAlias('PommProject\Foundation\Pomm', new Alias('pomm', false));
         $container->setParameter('pomm.configuration', $config['configuration']);
 
         if (isset($config['logger']['service'])) {
